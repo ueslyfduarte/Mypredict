dce2f96df6b62dcf27eedf55b8df9ecf55cdcb08
ueslyfduarte <ueslyfduarte@gmail.com>
2026-07-23 20:25:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,3 @@ else:
 BASE_URL = "https://api-sports.io"
 
 
-# ---------------------------------------------------------------------
-# [MÓDULO 2] MONITOR DE CONSUMO DA API (TOPO DA PÁGINA)
-# ---------------------------------------------------------------------
-
-col_req1, col_req2 = st.columns(2)
-
-with col_req1:
-    st.metric(label="📊 Requisições Gastas Hoje", value=st.session_state.requisicoes_feitas)
-
-with col_req2:
-    st.metric(label="🚨 Limite do Seu Plano", value=st.session_state.limite_diario)
-
-st.divider()
-
-        st.rerun()
