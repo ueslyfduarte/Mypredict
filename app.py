ccaede48607a698f973f17386e9d0f7dec1494df
ueslyfduarte <ueslyfduarte@gmail.com>
2026-07-29 12:11:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,11 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="MyPredict 2.0", layout="wide", page_icon="⚽")
     st.sidebar.markdown("# MyPredict 2.0")
-    modo = st.sidebar.radio("Modo", ["Manual", "Manual com IA"])
+    modo = st.sidebar.radio("Modo", ["Manual"])
 
     if modo == "Manual":
         from ui.manual_page import render_manual
         render_manual()
-    else:
-        from ui.manual_ia_page import render_manual_ia
-        render_manual_ia()
 
 if __name__ == "__main__":
     main()
